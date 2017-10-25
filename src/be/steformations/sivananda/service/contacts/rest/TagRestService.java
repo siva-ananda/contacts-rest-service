636f31c47b531403e5b356a4518b33cb8fa5ac6b
siva-ananda <dev.sivananda@gmail.com>
2017-10-25 13:46:59 +0200

    return tag as xml

--- src/be/steformations/sivananda/service/contacts/rest/TagRestService.java
+++ src/be/steformations/sivananda/service/contacts/rest/TagRestService.java
@@ -2,6 +2,8 @@ package be.steformations.sivananda.service.contacts.rest;
 
 import be.steformations.java_data.contacts.interfaces.beans.Tag;
 import be.steformations.java_data.contacts.interfaces.dao.TagDao;
+import be.steformations.sivananda.data.contacts.dto.ContactsDtoFactory;
+import be.steformations.sivananda.data.contacts.dto.TagDto;
 
 /*
 http://localhost:8080/contacts-rest/rs/tag
@@ -11,10 +13,12 @@ http://localhost:8080/contacts-rest/rs/tag
 public class TagRestService {
 
 	private TagDao tagDao;
+	private ContactsDtoFactory contactsDtoFactory;
 
 	public TagRestService() {
 		super();
 		this.tagDao = ContactRestDaoFactory.getInstance().getTagDao();
+		this.contactsDtoFactory = new ContactsDtoFactory();
 	}
 
 	// GET http://localhost:8080/contacts-rest/rs/tag/1/value
@@ -28,4 +32,22 @@ public class TagRestService {
 		}
 		return tag.getValue();
 	}
+
+	// GET http://localhost:8080/contacts-rest/rs/tag/1/value
+	@javax.ws.rs.GET
+	@javax.ws.rs.Path("{pk:[1-9]+}")
+	@javax.ws.rs.Produces(javax.ws.rs.core.MediaType.TEXT_PLAIN)
+	public String getTagByValueAsText(@javax.ws.rs.PathParam("pk") int id) {
+		String xml = null;
+		Tag tag = this.tagDao.getTagById(id);
+		TagDto dto = new TagDto();
+
+		if (tag != null) {
+			dto.setId(tag.getId());
+			dto.setValue(tag.getValue());
+		}
+		xml = this.contactsDtoFactory.tagToXml(dto);
+		return xml;
+	}
+
 }
